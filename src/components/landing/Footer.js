7ca8bb9c3a0e1c163536d8463d560c7823ea7689
dileepkumarkethe <63191518+dileepkumarkethe@users.noreply.github.com>
2020-11-13 01:17:28 +0530

    first commit

---
+++ src/components/landing/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2 className="display-4 text-center py-5 my-4">Features</h2>
+
+            <nav className="nav justify-content-center nav-pills flex-column flex-md-row">
+                
+                <a className="nav-link" href="#others" data-toggle="tab">Others</a>
+            </nav>
+
+            <div className="tab-content py-5">
+                
+
+                <div className="tab-pane" id="others">
+                    <h3>Others</h3>
+                    <ul>
+                        <li>Redux</li>
+                        <li>Redux Form</li>
+                        <li>Lodash</li>
+                        <li>React Bootstrap Table</li>
+                        <li>Font Awesome (for icons)</li>
+                        <li>Hot Module Replacement (HMR)</li>                        
+                    </ul>
+                </div>                
+            </div>
+        </footer>
+    );
+};
+
+
+
+export default Footer;
---
+++ src/components/landing/Section.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import reactjs from '../../img/reactjs.jpg';
+import createReactApp from '../../img/createReactApp.jpg';
+import reactrouter4 from '../../img/reactrouter4.jpg';
+import bootstrap4 from '../../img/bootstrap4.jpg';
+
+
+
+const Section = () => {
+    return (
+
+        <section className="row">
+           
+
+        </section>
+    );
+};
+
+
+
+export default Section;
